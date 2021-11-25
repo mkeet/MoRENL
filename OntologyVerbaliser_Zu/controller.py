960135d6c559be57aced1128d9fee72823faa835
Junior moraba <morabav@gmail.com>
2021-11-25 20:45:34 +0200

    modifiedZuluOntologyVerbaliser1stcom

---
+++ OntologyVerbaliser_Zu/controller.py
@@ -0,0 +1,135 @@
+from ontologyExtractor import OntologyExtractor
+from sentenceGenerator import evaluate
+
+WORDTYPE = ['n','v']
+
+TYPE1 = ['exists','enziwe','nexist','akhiwe','umunxa','disjoint',\
+    'ingxenye','ingxenye_s','hlanganyele']
+TYPE2 = ['subclasses','intersection','bbbb','ffff','aaaa','dddd','cccc','has']
+
+def formatType1(sentence, n, words):
+    end = len(words[0]); lst = []
+
+    for i in range(0, n):
+        start = end + 1
+        end = start + len(words[i+1])
+        lst.append([WORDTYPE[i%2], start, end])
+
+
+    if len(words) >4:
+        if ('dluli' in words[4] or 'phezu' in words[4]):
+            start = end+1
+            end = start+len(words[4])
+            lst.append(['a',start,end])
+        
+        elif (words[-1]=='ncamashi'):
+            start = sentence.find('ncamashi')
+            end = start+len(words[-1])
+            lst.append(['a',start,end])
+
+    return lst
+
+def formatType2(sentence, noun2, words):
+
+    if len(words) == 2:
+        start = 0; end = len(words[0]); index = 1
+    else:
+        start = len(words[0]) + 1
+        end = start + len(words[1])
+        index = 2
+
+    lst = [[WORDTYPE[0], start, end]]
+
+    if (noun2=='has'): #The verb is at the end of the "na" or whatever its coalscene was
+        
+        start = end + 1
+        length = len(words[1])
+        end = words[1].find('n')+2+start
+    else:
+        noun2 = noun2[1:]
+        length = len(noun2)
+        wordlength = len(words[index])
+
+        if not words[index].endswith(noun2, 0, wordlength):
+            length += 2
+
+        isenzo = words[index][:wordlength-length]
+        start = sentence.find(isenzo, 0, len(sentence))
+        end = start + len(isenzo)
+
+
+    lst.append([WORDTYPE[1], start, end])
+    lst.append([WORDTYPE[0], end, end + length])
+
+    return lst
+
+def getFormats(sentence, n):
+    words = sentence.split(' ')
+
+    if type(n) == str:
+        return formatType2(sentence, n, words)
+    elif n == 0:
+        return [['t', 0, len(sentence)]]
+    elif n == -1:
+        return []
+    else:
+        return formatType1(sentence, n, words)
+
+def loadOntology(path, iri):
+    try:
+        onto_extractor = OntologyExtractor(path, iri)
+    except Exception as e:
+        return False
+
+    return True
+
+def printlog(title, errors, descr):
+    with open('Error.log','a') as logfile:
+        print(title, descr, file=logfile)
+        [print(e, file=logfile) for e in errors]
+        print('',file=logfile)
+
+def printResult(path, iri):
+    onto_extractor = OntologyExtractor(path, iri)
+    onto_extractor.extract()
+
+    for (k, v) in onto_extractor.my_dict.items():
+        for i in v:
+            [r, e] = evaluate(k, i)
+            if e != None:
+                print(e)
+            else:
+                print(r)
+        print()
+
+def storeResult(result, errors, body, i, k):
+    [r, e] = result
+    if e != None:
+        errors.append(e)
+    elif k in TYPE1:
+        body.append([r, 3])
+    elif k in TYPE2:
+        body.append([r, i[1]])
+    else:
+        body.append([r, 0])
+
+def data(path, iri):
+    onto_extractor = OntologyExtractor(path, iri)
+    onto_extractor.extract()
+    result = []
+
+    for (k, v) in onto_extractor.my_dict.items():
+        body = []; errors = []
+        
+
+        [storeResult(evaluate(k, i), errors, body, i, k) for i in v]
+
+        if len(body) > 0:
+            result = result + [[k, 0]] + body + [['', -1]]
+            
+
+        diff = len(v) - len(body)
+        if len(errors) != 0:
+            printlog(k, errors,"{d}:{t}".format(d=str(diff), t=str(len(v))))
+
+    return result
